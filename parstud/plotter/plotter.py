7468d13b09fc77ffd317668954fd2407ccf97030
mrovirasacie <marcrovirasacie@gmail.com>
2019-10-28 12:15:50 +0100

    Updated structure for plotter and reader

---
+++ parstud/plotter/plotter.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+if __name__ == "__main__":
+    
+    path = "tests/input/logs_test/logs.csv"
+    df = pd.read_csv(path)
+    
+
+
+    pass
